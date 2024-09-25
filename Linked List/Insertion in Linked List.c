5d83f88e90addbe9809eb58f04b136e58bdecb2b
Tapoban Ray <107751519+tapoban123@users.noreply.github.com>
2024-09-26 00:38:37 +0530

    Updated Insertion file of Linked List

--- Linked List/Insertion in Linked List.c
+++ Linked List/Insertion in Linked List.c
@@ -11,7 +11,8 @@ int countAllNodes(struct Node * head) {
 	int count = 0;
 	temp = head;
 
-	while (temp -> next != 0) {
+	while (temp != 0) {
+		temp = temp -> next;
 		count++;
 	}
 
@@ -53,23 +54,21 @@ void insertAtEnd(struct Node * head) {
 
 void insertAfterSpecificPosition(struct Node *head, int pos) {
 	struct Node * temp, *newNode;
-	int count = 0, numOfNodes;
+	int count = 1, numOfNodes;
 
-	numOfNodes = countAllNodes(head);
+	temp = head;
+	newNode = (struct Node *)(malloc(sizeof(struct Node)));
 
-	if (pos > 0 && pos <= numOfNodes) {
-		newNode = (struct Node *)(malloc(sizeof(struct Node)));
+	printf("Enter data for new node: ");
+	scanf("%d", &newNode -> data);
+	newNode -> next = 0;
 
-		while (count < pos) {
-			temp = temp -> next;
-			count++;
-		}
-		newNode -> next = temp -> next;
-		temp -> next = newNode;
-	} else {
-		printf("Invalid position.");
-
+	while (count < pos) {
+		temp = temp -> next;
+		count++;
 	}
+	newNode -> next = temp -> next;
+	temp -> next = newNode;
 }
 
 void displayAllNodes(struct Node * head) {
@@ -77,11 +76,11 @@ void displayAllNodes(struct Node * head) {
 	int numOfNodes;
 
 	numOfNodes = countAllNodes(head);
-	temp = head;
 
 	if (numOfNodes == 0) {
 		printf("No data present in Linked List.");
 	} else {
+		temp = head;
 		printf("The values present in the Linked List: ");
 		while (temp != 0) {
 			printf("%d ", temp -> data);;
@@ -96,7 +95,8 @@ void menu() {
 	printf("\nEnter 2 to add a node at the end.");
 	printf("\nEnter 3 to add a node after a given position.");
 	printf("\nEnter 4 to display data of all the nodes.");
-	printf("\nEnter 5 to Exit.");
+	printf("\nEnter 5 to view the number of nodes.");
+	printf("\nEnter 6 to Exit.");
 }
 
 int main() {
@@ -111,10 +111,11 @@ int main() {
 	head -> next = 0;
 
 	menu();
-	numOfNodes = countAllNodes(head);
 
 	while (choice > 0 || choice <= 5) {
-		printf("\n\nEnter your choice: ");
+		numOfNodes = countAllNodes(head);
+
+		printf("\nEnter your choice: ");
 		scanf("%d", &choice);
 
 		if (choice == 1) {
@@ -127,12 +128,21 @@ int main() {
 			} else {
 				printf("Enter the position where you want to insert: ");
 				scanf("%d", &pos);
+				printf("%d", numOfNodes);
+				if (pos > 0 && pos <= numOfNodes) {
 
-				insertAfterSpecificPosition(head, pos);
+					insertAfterSpecificPosition(head, pos);
+				} else {
+					printf("Invalid position.");
+
+				}
+
 			}
 		} else if (choice == 4) {
 			displayAllNodes(head);
-		} else if (choice == 5) {
+		} else if(choice ==5) {
+			printf("Number of nodes present in the Linked List = %d", countAllNodes(head));
+		} else if (choice == 6) {
 			printf("Program executed Successfully.");
 			break;
 		} else {
@@ -141,6 +151,3 @@ int main() {
 		}
 	}
 }
-
-
-
