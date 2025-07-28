f197fec6507d751909bd8c4c706395c6e1d30945
Tapoban Ray <raytapoban@gmail.com>
2025-07-28 19:29:31 +0530

    Programmed fifteenth pattern

---
+++ pattern_printing/pattern_15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char alphabets[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    int n;
+    printf("Enter the number of lines: ");
+    scanf("%d", &n);
+
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c", alphabets[j]);
+        }
+
+        printf("\n");
+    }
+}
