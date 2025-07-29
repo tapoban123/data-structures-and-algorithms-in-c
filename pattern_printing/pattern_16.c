c4eb88d544db10355bfc34c10a8fb79e0f7e15e3
Tapoban Ray <raytapoban@gmail.com>
2025-07-29 19:56:54 +0530

    Implemented sixteenth pattern

---
+++ pattern_printing/pattern_16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char alphabets[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    int n;
+    printf("Enter the number of lines: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c", alphabets[i - 1]);
+        }
+        printf("\n");
+    }
+}
