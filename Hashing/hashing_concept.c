5913b9ae39039e06be08ea7ba1304ea52d595022
Tapoban Ray <raytapoban@gmail.com>
2025-08-05 01:24:58 +0530

    Implemented the concept of Hashing

---
+++ Hashing/hashing_concept.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100] = {0};
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int inputArr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter an element between 0 and 100: ");
+        scanf("%d", &inputArr[i]);
+        arr[inputArr[i]]++;
+    }
+
+    printf("\nElements: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", inputArr[i]);
+    }
+
+    int query;
+    int keepRunning = 1;
+
+    while (keepRunning)
+    {
+        printf("\nEnter the element whose count you want to know: ");
+        scanf("%d", &query);
+
+        printf("%d is present %d times in the array.", query, arr[query]);
+
+        printf("Do you want to continue? (0/1) ");
+        scanf("%d", &keepRunning);
+    }
+}
