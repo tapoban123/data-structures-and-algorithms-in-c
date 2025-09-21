ada76b62bed274ca8f502e1928145ca7c185df89
Tapoban Ray <tapoban.ray@novaryd.com>
2025-09-22 02:03:40 +0530

    Working on LongestSubarrayWithSumk algorithm

---
+++ Array_Problems/longest_subarray_with_sum_k.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> finaLongestSubArray(vector<int> arr)
+{
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, 4, 7};
+    int k = 4;
+
+    
+}
