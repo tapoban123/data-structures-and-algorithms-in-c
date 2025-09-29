9b4d7699e91db2949e50ddb71acd901fdfe992ba
Tapoban Ray <raytapoban@gmail.com>
2025-09-29 14:45:06 +0530

    Implemented SelectionSort with BinarySearch

--- Binary_Search_Problems/binary_search_to_find_x.cpp
+++ Binary_Search_Problems/binary_search_to_find_x.cpp
@@ -2,6 +2,29 @@
 #include <vector>
 using namespace std;
 
+vector<int> SelectionSort(vector<int> arr)
+{
+    int temp, min;
+    for (int i = 0; i < (int)arr.size() - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < (int)arr.size(); j++)
+        {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+
+        if (min != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    return arr;
+}
+
 int BinarySearch(vector<int> arr, int k)
 {
     int start = 0, end = arr.size(), mid;
@@ -31,11 +54,14 @@ void displayArr(vector<int> arr)
 int main()
 {
     int key;
-    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    vector<int> arr = {1, 6, 3, 4, 5, 2, 1, 2};
+
+    arr = SelectionSort(arr);
     displayArr(arr);
+
     cout << "Enter an element: ";
     cin >> key;
-
+    
     int keyFound = BinarySearch(arr, key);
     if (keyFound == -1)
         cout << key << " not found in array.";
