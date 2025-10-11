c37b8c5dfe84ad696ee5ffe063f34c40997bd6e8
Tapoban Ray <raytapoban@gmail.com>
2025-10-11 10:50:52 +0530

    Implemented algorithm to search element in a rotated array

---
+++ Binary_Search_Problems/search_element_in_rotated_array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchElementInRotatedArr(vector<int> arr, int k)
+{
+    int low = 0, high = arr.size() - 1, mid;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (arr[mid] == k)
+            return mid;
+        else if (arr[low] <= arr[mid])
+        {
+            // Left is sorted
+            if (arr[low] <= k && k <= arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+        {
+            // Right is sorted
+            if (arr[mid] <= k && k <= arr[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+void displayArr(vector<int> arr)
+{
+    cout << "Elements: ";
+    for (int i : arr)
+        cout << i << " ";
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int k;
+
+    displayArr(arr);
+    cout << "Enter element to search: ";
+    cin >> k;
+
+    int index = searchElementInRotatedArr(arr, k);
+
+    if (index == -1)
+        cout << k << " not found in the given array.";
+    else
+        cout << k << " is found at index " << index;
+}
